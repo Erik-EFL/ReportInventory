8b8c413c03cd79656482e72497e5ef5b8714dcd3
erik-efl <erik.lima.f@outlook.com>
2022-10-28 19:20:34 -0300

    test: :test_tube: Adding a new test
    Create a test for product report

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,28 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    id = 1
+    nome_do_produto = "Café"
+    nome_da_empresa = "Nescafé"
+    data_de_fabricacao = "2020-01-01"
+    data_de_validade = "2021-01-01"
+    numero_de_serie = "123456789"
+    instrucoes_de_armazenamento = "em local seco e fresco"
+    message = (
+        f"O produto {nome_do_produto} fabricado em {data_de_fabricacao} "
+        f"por {nome_da_empresa} com validade até {data_de_validade} "
+        f"precisa ser armazenado {instrucoes_de_armazenamento}."
+    )
+
+    product = Product(
+        id,
+        nome_do_produto,
+        nome_da_empresa,
+        data_de_fabricacao,
+        data_de_validade,
+        numero_de_serie,
+        instrucoes_de_armazenamento,
+    )
+
+    assert repr(product) == message
